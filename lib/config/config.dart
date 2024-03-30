763a5aa5ce8e5ed9e560de72f5f234842c21397a
MDSHAKHAWAT <sakawat.hossain.uk@outlook.com>
2024-03-30 17:44:54 +0800

    update api url

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  static String baseUrl = 'gamereviewapp.xyz';
+  static String baseUrl = 'moderator.gamereviewapp.xyz';
   static Map<String, String> header = {'Accept': 'application/json'};
 }
