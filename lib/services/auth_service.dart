43bc1507df31246d9baa5299ea6da1948ef09f1d
MDSHAKHAWAT <sakawat.hossain.uk@outlook.com>
2024-03-30 18:30:44 +0800

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -25,7 +25,6 @@ class AuthService {
     header['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2xvZ2luIiwiaWF0IjoxNzEwOTYxNjQ2LCJleHAiOjE3MTA5NjUyNDYsIm5iZiI6MTcxMDk2MTY0NiwianRpIjoiTzBXUWRicHMyTWE3enR3SCIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.7y7eg7QLuUtHPAxSnaN30z3J6yF1ukAuzAxTyOEkzCA';
     
     var response = await http.post(url, body: formData, headers: header);
-    print('Response body: ${response.body}');
     return response.body;
   }
   
